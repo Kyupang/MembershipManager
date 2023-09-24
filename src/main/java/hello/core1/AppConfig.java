f5b512ad1fc021acae5711aacfa9df802224b162
KimKyuHwan <vkdls754@gmail.com>
2023-09-24 19:10:36 +0900

    no message

--- src/main/java/hello/core1/AppConfig.java
+++ src/main/java/hello/core1/AppConfig.java
@@ -6,23 +6,27 @@ import hello.core1.member.MemberServiceImpl;
 import hello.core1.member.MemoryMemberRepository;
 import hello.core1.order.OrderService;
 import hello.core1.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     // 서비스는 생성자를 통해 주입받는다.
     // 여기서 생성해줌
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), DiscountPolicy());
     }
-
-    private static RateDiscountPolicy DiscountPolicy() {
+    @Bean
+    public RateDiscountPolicy DiscountPolicy() {
         return new RateDiscountPolicy();
     }
 
